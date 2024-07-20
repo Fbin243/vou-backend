ddb88a0019c9d0c38c49ae27711fa8c69da072d4
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-07-20 23:19:35 +0700

    feat: add eureka server as service discovery

--- services/games/src/main/java/com/vou/games/GamesApplication.java
+++ services/games/src/main/java/com/vou/games/GamesApplication.java
@@ -2,6 +2,7 @@ package com.vou.games;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
