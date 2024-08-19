9083f269333f14f77cb4603bffadcb5b270ee113
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-08-19 12:59:29 +0700

    feat: add basic gateway

---
+++ services/gatewayserver/src/test/java/com/vou/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vou.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
