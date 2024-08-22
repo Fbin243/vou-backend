9e81a217a1a82bad744974c6e89c73dca758c791
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-08-22 15:57:21 +0700

    chore: add observability

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 images=(
-#  "discovery-server:0.0.1"
-  "api-gateway:0.0.1"
-#  "identity:0.0.1"
-#  "users:0.0.1"
-#  "games:0.0.1"
-  "sessions:0.0.1"
+#  "discovery-server"
+  "api-gateway"
+#  "identity"
+#  "users"
+#  "games"
+#  "sessions"
 )
 
-cd services/discovery-server/; mvn compile jib:dockerBuild; cd ..; cd ..
-cd services/api-gateway/; mvn compile jib:dockerBuild; cd ..; cd ..
-cd services/identity/; mvn compile jib:dockerBuild; cd ..; cd ..
-cd services/users/; mvn compile jib:dockerBuild; cd ..; cd ..
-cd services/games/; mvn compile jib:dockerBuild; cd ..; cd ..
-cd services/sessions/; mvn compile jib:dockerBuild; cd ..; cd ..
-
-#for img in "${images[@]}"; do
+for img in "${images[@]}"; do
+  docker rmi fbin243/${img}:0.0.1
+  cd services/${img}/; mvn compile jib:dockerBuild; cd ..; cd ..
 #  docker push fbin243/${img}
-#done
+done
 
