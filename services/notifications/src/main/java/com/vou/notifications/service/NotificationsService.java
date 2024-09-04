e67a761631749c6314df294ef32ebc9c6bacf90e
Hồ Nhân <104899198+NhanHoPeter217@users.noreply.github.com>
2024-09-04 15:21:49 +0700

    feature/honhan-statistics-service: temp done for kafka, wait for testing

--- services/notifications/src/main/java/com/vou/notifications/service/NotificationsService.java
+++ services/notifications/src/main/java/com/vou/notifications/service/NotificationsService.java
@@ -8,32 +8,45 @@ import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.cloud.firestore.Firestore;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.vou.notifications.config.FirebaseConfig;
+import com.vou.notifications.consumer.NotificationConsumerService;
 import com.vou.notifications.common.ActiveStatus;
 import com.vou.notifications.dto.NotificationDto;
+import com.vou.notifications.entity.NotificationRelatedPair;
+import com.vou.notifications.model.NotificationInfo;
+import com.vou.notifications.repository.NotificationRelatedPairRepository;
 import com.vou.notifications.repository.NotificationRepository;
 import com.vou.notifications.repository.NotificationUserRepository;
+import com.vou.notifications.repository.UserTokenRepository;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Logger;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class NotificationsService implements INotificationsService {
-    private static final Logger                 log = LoggerFactory.getLogger(NotificationsService.class);
+    // private static final Logger                 log = LoggerFactory.getLogger(NotificationsService.class);
 
     private final FCMService                    fcmService;
     private final NotificationRepository        notificationRepository;
     private final NotificationUserRepository    notificationUserRepository;
+    private final NotificationRelatedPairRepository notificationRelatedPairRepository;
+    private final UserTokenRepository           userTokenRepository;
     private final FirebaseConfig                firebaseConfig;
 
     private final Firestore                     firestore;
+
+    private static final Logger logger = Logger.getLogger(NotificationConsumerService.class.getName());
 
     @Override
     public String createNotification(NotificationDto notificationInfo) {
@@ -68,6 +81,31 @@ public class NotificationsService implements INotificationsService {
         }
 
         return notificationId;
+    }
+
+    @Async
+    public CompletableFuture<Void> processUserNotificationAsync(String userId, NotificationInfo notificationDto) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                List<NotificationRelatedPair> relatedPairs = notificationRelatedPairRepository.findByNotificationId(notificationDto.getId());
+                String token = null;
+                token = userTokenRepository.findTokenByUserId(userId);
+    
+                if (token != null) {
+                    Map<String, String> data = new HashMap<>();
+                    for (NotificationRelatedPair relatedPair : relatedPairs) {
+                        data.put(relatedPair.getId().getRelatedKey(), relatedPair.getId().getRelatedId());
+                    }
+        
+                    fcmService.sendNotification(token, notificationDto, data);
+                    logger.info("Notification sent to userId: " + userId);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error processing notification for userId: " + userId, e);
+            }
+        });
     }
 
     private void saveUserNotificationToFirestore(String notificationId, String userId) {
---
+++ services/statistics/src/main/java/com/vou/statistics/kafka/deserializer/TransactionDeserializer.java
@@ -0,0 +1,20 @@
+package com.vou.statistics.kafka.deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vou.statistics.dto.TransactionDto;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class TransactionDeserializer implements Deserializer<TransactionDto> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public TransactionDto deserialize(String topic, byte[] data) {
+        try {
+            return objectMapper.readValue(data, TransactionDto.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error deserializing TransactionDto", e);
+        }
+    }
+}
