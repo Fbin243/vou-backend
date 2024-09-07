80b8469c5a8a031324d5a84c4dca3d7bb6ce41e0
Hồ Nhân <104899198+NhanHoPeter217@users.noreply.github.com>
2024-09-07 11:45:24 +0700

    try to fix transaction saved

--- services/statistics/src/main/java/com/vou/statistics/StatisticsApplication.java
+++ services/statistics/src/main/java/com/vou/statistics/StatisticsApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableMongoRepositories
+@EnableJpaRepositories
 @EnableFeignClients
 public class StatisticsApplication {
 
