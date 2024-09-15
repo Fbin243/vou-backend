1576c67ee311f76f6f999fd952ea56ffbf6dbb00
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-09-15 12:45:44 +0700

    chore: remove .vscode

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 images=(
-#  "discovery-server"
+  "discovery-server"
   "api-gateway"
   "identity"
   "users"
-#  "games"
-#  "events"
-#  "sessions"
-#  "notifications"
+  "games"
+  "events"
+  "sessions"
+  "notifications"
 ) # Adjust this list base on purpose
 
 # Stop/Remove all containers
 cd docker-compose; docker compose down; cd ..
 
-#mvn clean
+mvn clean
 
 for img in "${images[@]}"; do
   # Remove the old images
-  docker rmi haina2410/${img}
+  docker rmi fbin243/${img}
 
   # Build new image with latest tag
   cd services/${img}/; mvn compile jib:dockerBuild; cd ..; cd ..
 
   # Push image to docker hub (just use for update)
-  docker push haina2410/${img}
+  docker push fbin243/${img}
 done
 
 # Run all of containers
-#cd docker-compose; docker compose up -d; cd ..
-
+cd docker-compose; docker compose up -d; cd ..
